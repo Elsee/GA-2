3838ed35df683abe9289e8d72bcf067e3a710656
Elsee <svetlana_else@mail.ru>
2016-11-14 21:17:19 +0300

    Project start

---
+++ src/Task1/MainT1.java
@@ -0,0 +1,10 @@
+package Task1;
+
+/**
+ * Created by svetl on 14.11.2016.
+ */
+public class MainT1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/Task2/MainT2.java
@@ -0,0 +1,10 @@
+package Task2;
+
+/**
+ * Created by svetl on 14.11.2016.
+ */
+public class MainT2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
